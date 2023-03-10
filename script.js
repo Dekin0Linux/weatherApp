2a40978864443de755a9e433fe6f63c075466426
dekin <dekinlinux@gmail.com>
2023-03-10 18:58:08 +0000

    new commit

--- script.js
+++ script.js
@@ -1,35 +1,21 @@
 const key = '2c06f04413a5cc6213b1567db6a83563';
 
-//Get Elements
-let currentCity = document.getElementById('city')
-let icon = document.querySelector('.img')
-let temp = document.getElementById('temp')
-let gen = document.getElementById('general')
-let humidity = document.getElementById('humidity')
-let tempMax = document.getElementById('tempMax')
-let tempMin = document.getElementById('tempMin')
-
 let features = document.querySelector('.features')
-let content = document.querySelector('.content')
+let content = document.querySelector('.content') //div to hold results
 
 // let form = document.getElementById('formData')
 let btn = document.getElementById('search')
 let city = document.getElementById('cityName')
-
-// 
-
 
 const getData=(e)=>{
     e.preventDefault()
     let cityVal = city.value
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityVal}&appid=${key}&units=metric`
 
-  
     city.value=""
     fetch(url)
         .then(res => res.json())
         .then(data => {
-            console.log(data)
             content.innerHTML=`<h1 class="cityname">${data.name}</h1>
             <img src="https://openweathermap.org/img/w/${data.weather[0].icon}.png" alt="icon"/>
 
@@ -57,11 +43,7 @@ const getData=(e)=>{
         })
         .catch(()=>{
             content.innerHTML='<h2>City Not Found</h2>'
-            city.value = ''
         })
-    
-    
-    
 }
 
 btn.addEventListener('click',getData)
